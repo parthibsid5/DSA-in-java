11502ac8990a2d53b25287231e2d6dc4020e440d
Parthib Sarkar <parthibsid5@gmail.com>
2024-09-14 01:33:12 +0530

    minor changes

--- 02_sorting/merge.java
+++ 02_sorting/merge.java
@@ -1,5 +1,6 @@
+// TC = n(Log n)
+// One drawbacks is that at each conquer step we have to create an array 
 public class merge {
-   public static int mid;
     public static void main(String args[]){
         int arr[]={6,3,9,5,2,8};
         int n=arr.length;
@@ -7,8 +8,9 @@ public class merge {
         for(int i=0;i<n;i++){
         System.out.println(arr[i]);
          }
-         }
+        }
     public static void divide(int arr[],int s,int e){
+        int mid;
         if(s>=e){ 
             return;
         }
@@ -19,23 +21,31 @@ public class merge {
         conquer(arr,s,mid,e);
     }
     public static void conquer(int arr[],int s,int mid,int e){
-        int merged[]=new int[e-s+1];
+        int merged[]=new int[e - s + 1];
         int idx1=s;
-        int idx2=mid;
+        int idx2=mid+1;
         int x=0;
         while(idx1<=mid && idx2<=e){
             if(arr[idx1]<=arr[idx2]){
-                merged[x++]=arr[idx1++];
+                merged[x]=arr[idx1];
+                x+=1;
+                idx1+=1;
             }
             else{
-                merged[x++]=arr[idx2++];
+                merged[x]=arr[idx2];
+                x+=1;
+                idx2+=1;
             }
         }
-        while(idx1<mid){
-            merged[x++]=arr[idx1++];
+        while(idx1<=mid){
+            merged[x]=arr[idx1];
+            x+=1;
+            idx1+=1;
         }
-        while(idx2<e){
-        merged[x++]=arr[idx2++];
+        while(idx2<=e){
+        merged[x]=arr[idx2];
+        x+=1;
+        idx2+=1;
     }
 
     for(int i=0,j=s;i<merged.length;i++,j++){
