80b090cb2bdfa7b553928d63fc2457fc5afce7f1
Parthib Sarkar <parthibsid5@gmail.com>
2024-09-21 20:15:28 +0530

    add quick sort in java

---
+++ 02_sorting/quick.java
@@ -0,0 +1,38 @@
+public class quick {
+    public static void main(String args[]) {
+        int[] arr = { 6, 3, 9, 5, 2, 8 };
+        int n = arr.length;
+        quickSort(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + "\n");
+        }
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pivotIdx = partition(arr, low, high);
+            quickSort(arr, low, pivotIdx - 1);
+            quickSort(arr, pivotIdx + 1, high);
+        }
+    }
+
+    public static int partition(int arr[], int low, int high) {
+        // assuming the last as pivot
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    }
+}
