d610f63575391b69267d735d41c2976fc0ebcd17
Parthib Sarkar <parthibsid5@gmail.com>
2024-10-26 02:11:39 +0530

    modified with circular queue

--- 04_dataStructures/Queue.java
+++ 04_dataStructures/Queue.java
@@ -59,21 +59,80 @@ public class Queue {
        
     }
     public static void main(String args[]) {
-        queueclass qc = new queueclass(5);
-        queueclass.addElement(1);
-        queueclass.addElement(2);
-        queueclass.addElement(3);
-        queueclass.addElement(4);
-        queueclass.addElement(5);
-        
-        System.out.println(queueclass.dequeue());
-        System.out.println(queueclass.dequeue());
-        System.out.println(queueclass.dequeue());
-        System.out.println(queueclass.dequeue());
-        System.out.println(queueclass.dequeue());
-        System.out.println(queueclass.peek());
+        // queueclass qc = new queueclass(5);
+        // queueclass.addElement(1);
+        // queueclass.addElement(2);
+        // queueclass.addElement(3);
+        // queueclass.addElement(4);
+        // queueclass.addElement(5);
+
+        // System.out.println(queueclass.dequeue());
+        // System.out.println(queueclass.dequeue());
+        // System.out.println(queueclass.dequeue());
+        // System.out.println(queueclass.dequeue());
+        // System.out.println(queueclass.dequeue());
+        // System.out.println(queueclass.peek());
+
+      circularqueue cq=new circularqueue(5);
+      cq.add(2);
+      cq.add(9);
+      cq.add(3);
+      cq.add(5);
+      cq.add(4);
+      cq.deque();
+      cq.deque();
+      cq.add(100);
+      cq.add(200);
+      cq.deque();
+      cq.deque();
+      cq.deque();
+      cq.deque();
+      cq.deque();
+      
+    //   cq.peek();
+
     }
 
+    static class circularqueue{
+        int size;
+        int arr[];
+        int rear=-1,front=-1;
+        circularqueue(int n){
+            arr=new int[n];
+            size=n;
+        }
+        public void add(int data){
+            if((rear+1)%size ==front){
+                System.out.println("CQ is full !");
+                return;
+            }
+            if(front==-1){
+                front=0;
+            }
+            rear=(rear+1) % size;
+            arr[rear]=data;
+            System.out.println(arr[rear]+" added");
+        }
+        public void deque(){
+            if(rear==-1 && front==-1){
+                System.out.println("CQ is empty !");
+                return;
+            }
+        // System.out.println(size);
+        System.out.println(arr[front]+" removed");
+        if(front==rear){
+            front=rear=-1;
+        }
+        else{
+        front=(front+1)%size;
+        }}
+        public void peek(){
+            if(front==-1){
+                System.out.println("CQ is empty !");
+            }
+            System.out.println(arr[front]);
+        }
+    }
 
     
 }
