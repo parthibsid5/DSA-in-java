3dfc40d0ef6b9158cba8d9bb20ada714dc696004
Parthib Sarkar <parthibsid5@gmail.com>
2024-11-08 12:24:51 +0530

    Treees in ds,  create tree, tree traversals- pre, in, post, level (bfs)

---
+++ 05_trees/Treeclass.java
@@ -0,0 +1,104 @@
+import java.util.*;
+public class  Treeclass{
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class binaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+    // root -> Ltree -> Rtree
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        System.out.print("-1 ");
+        preorder(root.right);
+    }
+
+    // Ltree -> root -> Rtree
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data+" ");
+        inorder(root.right);
+
+    }
+    // Ltree -> Rtree- > root
+    public static void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data+" ");
+    }
+//  using queue LL so add() func would add node into it!
+    public static void levelorder(Node root){
+        if(root==null){
+            return;
+        }
+        Queue <Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currNode=q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty())
+                {
+                    break;}
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.print(currNode.data+" ");
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String args[]) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        binaryTree bt = new binaryTree();
+        Node root = bt.buildTree(nodes);
+        // System.out.println(root.left.right.data);
+
+        Treeclass ct=new Treeclass();
+        // ct.preorder(root);
+        // ct.inorder(root);
+        // ct.postorder(root);
+        ct.levelorder(root);
+    }
+}
