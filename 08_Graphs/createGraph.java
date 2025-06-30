b9c831b7eed54171d2091644e8af1424a9650bd8
Parthib <parthibsid5@gmail.com>
2025-06-30 20:32:43 +0530

    disjointed bfs traversal

--- 08_Graphs/createGraph.java
+++ 08_Graphs/createGraph.java
@@ -19,42 +19,73 @@ class createGraph {
             graph[i] = new ArrayList<Edge>();
         }
         // manually adding
+        // graph[0].add(new Edge(0, 1, 2));
+        // graph[0].add(new Edge(0, 2, 3));
+
+        // graph[1].add(new Edge(1, 3, 10));
+
+        // graph[2].add(new Edge(2, 4, 2));
+
+        // graph[3].add(new Edge(3, 4, 0));
+        // graph[3].add(new Edge(3, 5, -1));
+
+        // graph[4].add(new Edge(4, 3, -1));
+        // graph[4].add(new Edge(4, 5, -1));
+
+        // graph[5].add(new Edge(5, 6, -10));
+
+        // graph[6].add(new Edge(6, 5, 2));
+
+
         graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 3));
 
-        graph[1].add(new Edge(1, 3, 10));
+        graph[1].add(new Edge(1, 0, 2));
+        graph[1].add(new Edge(1, 2, 2));
 
-        graph[2].add(new Edge(2, 4, 2));
+        graph[2].add(new Edge(2, 1, 2));
 
-        graph[3].add(new Edge(3, 4, 0));
-        graph[3].add(new Edge(3, 5, -1));
+        graph[3].add(new Edge(3, 4, 2));
 
-        graph[4].add(new Edge(4, 3, -1));
-        graph[4].add(new Edge(4, 5, -1));
-
-        graph[5].add(new Edge(5, 6, -10));
-
-        graph[6].add(new Edge(6, 5, 2));
+        graph[4].add(new Edge(4, 3, 2));
 
     }
-    public static void BFS(ArrayList<Edge> graph[],int v){
-        java.util.Queue<Integer> q=new LinkedList<>();
-        boolean visited[]=new boolean[v];
-        for(int i=0;i<graph.length;i++){
-            visited[i]=false;
+
+    public static void BFS(ArrayList<Edge> graph[], int v) {
+        java.util.Queue<Integer> q = new LinkedList<>();
+        boolean visited[] = new boolean[v];
+        for (int i = 0; i < graph.length; i++) {
+            visited[i] = false;
         }
         q.add(graph[0].get(0).src);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if (visited[curr] == false) {
+                System.out.println(curr);
+                visited[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void BFS_disjointed(ArrayList<Edge> graph[], boolean visit[], int start) {
+        java.util.Queue<Integer> q = new LinkedList<>();
+
+        q.add(start);
         while(!q.isEmpty()){
             int curr=q.remove();
-            if(visited[curr]==false){
+            if(visit[curr]==false){
                 System.out.println(curr);
-                visited[curr]=true;
+                visit[curr]=true;
                 for(int i=0;i<graph[curr].size();i++){
                     Edge e=graph[curr].get(i);
                     q.add(e.dest);
                 }
             }
         }
+
     }
 
     public static void main(String a[]) {
@@ -68,7 +99,13 @@ class createGraph {
                 // System.out.println(e.src + "--->" + e.dest + ": " + e.weight);
             }
         }
-        BFS(graph,v);
+        // BFS(graph,v);
 
+        // bfs disjointed the disjointed algo can be used for both jointed . disjointed!
+        boolean visit[] = new boolean[v];
+        // int start = graph[0].get(0).src;
+        for(int i=0;i<v;i++){
+            BFS_disjointed(graph, visit, i);            
+        }
     }
 }
