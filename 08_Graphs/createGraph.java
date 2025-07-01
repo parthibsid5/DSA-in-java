5895b0bd9523357f38762f7853551928410b10b6
Parthib <parthibsid5@gmail.com>
2025-07-01 16:38:48 +0530

    added dfs disjointed traversal

--- 08_Graphs/createGraph.java
+++ 08_Graphs/createGraph.java
@@ -19,34 +19,34 @@ class createGraph {
             graph[i] = new ArrayList<Edge>();
         }
         // manually adding
-        // graph[0].add(new Edge(0, 1, 2));
-        // graph[0].add(new Edge(0, 2, 3));
-
-        // graph[1].add(new Edge(1, 3, 10));
-
-        // graph[2].add(new Edge(2, 4, 2));
-
-        // graph[3].add(new Edge(3, 4, 0));
-        // graph[3].add(new Edge(3, 5, -1));
-
-        // graph[4].add(new Edge(4, 3, -1));
-        // graph[4].add(new Edge(4, 5, -1));
-
-        // graph[5].add(new Edge(5, 6, -10));
-
-        // graph[6].add(new Edge(6, 5, 2));
-
-
         graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 3));
 
-        graph[1].add(new Edge(1, 0, 2));
-        graph[1].add(new Edge(1, 2, 2));
+        graph[1].add(new Edge(1, 3, 10));
 
-        graph[2].add(new Edge(2, 1, 2));
+        graph[2].add(new Edge(2, 4, 2));
 
-        graph[3].add(new Edge(3, 4, 2));
+        graph[3].add(new Edge(3, 4, 0));
+        graph[3].add(new Edge(3, 5, -1));
 
-        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 3, -1));
+        graph[4].add(new Edge(4, 5, -1));
+
+        graph[5].add(new Edge(5, 6, -10));
+
+        graph[6].add(new Edge(6, 5, 2));
+
+
+        // graph[0].add(new Edge(0, 1, 2));
+
+        // graph[1].add(new Edge(1, 0, 2));
+        // graph[1].add(new Edge(1, 2, 2));
+
+        // graph[2].add(new Edge(2, 1, 2));
+
+        // graph[3].add(new Edge(3, 4, 2));
+
+        // graph[4].add(new Edge(4, 3, 2));
 
     }
 
@@ -88,8 +88,36 @@ class createGraph {
 
     }
 
+    // O(v+e)
+    public static void DFS(ArrayList<Edge> graph[],boolean dfs_visit[],int curr){
+        // if(curr==0){
+        //     return;
+        // }
+        System.out.println(curr);
+        dfs_visit[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e=graph[curr].get(i);
+            if(dfs_visit[e.dest]==false){
+                DFS(graph, dfs_visit, e.dest);
+            }
+        }
+    }
+
+    public static void DFS_disjoied(ArrayList<Edge> graph[],boolean dfs_dis_visit[],int curr){
+        System.out.println(curr);
+        dfs_dis_visit[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e=graph[curr].get(i);
+            if(dfs_dis_visit[curr]==false){
+                DFS_disjoied(graph, dfs_dis_visit, e.dest);
+            }
+        }
+
+    }
+
     public static void main(String a[]) {
         int v = 7;
+        // int v = 5;
         ArrayList<Edge> graph[] = new ArrayList[v];
         creategraph(graph);
         for (int i = 0; i < graph.length; i++) {
@@ -102,10 +130,22 @@ class createGraph {
         // BFS(graph,v);
 
         // bfs disjointed the disjointed algo can be used for both jointed . disjointed!
-        boolean visit[] = new boolean[v];
+        // boolean visit[] = new boolean[v];
         // int start = graph[0].get(0).src;
-        for(int i=0;i<v;i++){
-            BFS_disjointed(graph, visit, i);            
-        }
+        // for(int i=0;i<v;i++){
+            //     BFS_disjointed(graph, visit, i);            
+            // }
+            // boolean dfs_visit[] = new boolean[v];
+            // DFS(graph, dfs_visit, 0);
+
+            // dfs disjoined
+            boolean dfs_dis_visit[]=new boolean[v];
+            for(int i=0;i<v;i++){
+                if(dfs_dis_visit[i]==false){
+                    DFS_disjoied(graph, dfs_dis_visit, i);
+                }
+            }
+
     }
+    
 }
